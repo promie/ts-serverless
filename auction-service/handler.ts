c6f2f1b32c1eedca95266290d0c22ae40c4a650f
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-14 04:39:55 +1100

    SLS-01 implement logic to createAuction function

--- auction-service/handler.ts
+++ auction-service/handler.ts
@@ -1,6 +1,15 @@
-export async function createAuction(event, context) {
+export async function createAuction(event) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString()
+  }
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context })
+    statusCode: 201,
+    body: JSON.stringify(auction)
   };
 }
