76724186234971df84263fee2623639a73bd9a11
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-17 07:00:22 +1100

    SLS-17 finalising the dynamoDBClients module

--- auction-service/lib/dynamoDBClients.ts
+++ auction-service/lib/dynamoDBClients.ts
@@ -6,4 +6,4 @@ const region = "ap-southeast-2";
 const client = new DynamoDBClient({ region });
 const docClient = DynamoDBDocumentClient.from(client);
 
-export { client, docClient };
+export { docClient };
