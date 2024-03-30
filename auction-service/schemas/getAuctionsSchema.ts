8cc398f25759e7f0dbde1c55637d23908aabebbb
Promie Yutasane <promie.yutasane@gmail.com>
2024-03-31 06:36:25 +1100

    SLS-25 define getAuctionsSchema

---
+++ auction-service/schemas/getAuctionsSchema.ts
@@ -0,0 +1,16 @@
+const schema = {
+  properties: {
+    queryStringParameters: {
+      type: 'object',
+      properties: {
+        status: {
+          type: 'string',
+          enum: ['OPEN', 'CLOSED'],
+          default: 'OPEN',
+        },
+      },
+    },
+  },
+}
+
+export default schema
