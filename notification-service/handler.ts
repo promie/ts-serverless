1c67816f41547db12744b91828406ad2445358cb
Promie Yutasane <promie.yutasane@gmail.com>
2024-04-21 06:08:13 +1000

    SLS-39 initiating the SES client

--- notification-service/handler.ts
+++ notification-service/handler.ts
@@ -1,3 +1,7 @@
+import { SESClient } from "@aws-sdk/client-ses";
+
+const client = new SESClient({ region: "ap-southeast-2" });
+
 export async function sendMail(event) {
   console.log("event", event);
 
